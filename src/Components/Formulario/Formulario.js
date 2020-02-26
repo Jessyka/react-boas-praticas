999eed27c7451db236b00f078caf4b13ca91bd21
jessykalage <jessyka.l@thoughtworks.com>
2020-02-26 13:43:21 -0300

    create toast component

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import FormValidator from '../../utils/FormValidator';
-import PopUp from '../../utils/PopUp'
 import {TextField} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Toast from "../Toast/Toast";
 
 class Formulario extends Component {
 
     constructor(props) {
-        super(props);
+        super(props)
 
         this.validador = new FormValidator([
             {
@@ -26,7 +26,7 @@ class Formulario extends Component {
             {
                 campo: 'preco',
                 metodo: 'isInt',
-                args: [{min: 0, max: 99999}],
+                args: [{ min: 0, max: 99999 }],
                 validoQuando: true,
                 mensagem: 'Entre com um valor numérico'
             }
@@ -36,89 +36,113 @@ class Formulario extends Component {
             nome: '',
             livro: '',
             preco: '',
-            validacao: this.validador.valido()
+            validacao: this.validador.valido(),
+            mensagem: {
+                open: false,
+                texto: '',
+                tipo: ''
+            }
         }
 
-        this.state = this.stateInicial;
-
+        this.state = this.stateInicial
     }
 
     escutadorDeInput = event => {
-        const { name, value } = event.target;
+        const { name, value } = event.target
 
         this.setState({
             [name]: value
-        });
+        })
     }
 
     submitFormulario = () => {
-        const validacao = this.validador.valida(this.state);
+        const validacao = this.validador.valida(this.state)
 
-        if(validacao.isValid){
-            this.props.escutadorDeSubmit(this.state);
-            this.setState(this.stateInicial);
-        }else{
-            const { nome, livro, preco } = validacao;
-            const campos = [nome, livro, preco];
+        if (validacao.isValid) {
+            this.props.escutadorDeSubmit(this.state)
+            this.setState(this.stateInicial)
+        } else {
+            const { nome, livro, preco } = validacao
+            const campos = [nome, livro, preco]
             const camposInvalidos = campos.filter(elem => {
-                return elem.isInvalid;
-            });
-            camposInvalidos.forEach(campo => {
-                PopUp.exibeMensagem('error', campo.mensagem);
-            });
-        }
+                return elem.isInvalid
+            })
+            const erros = camposInvalidos.reduce(
+                (erros, campo) => erros + campo.mensagem + '. ',
+                ''
+            )
 
+            this.setState({
+                mensagem: {
+                    mensagem: erros,
+                    tipo: 'error',
+                    open: true
+                }
+            })
+        }
     }
 
     render() {
 
-        const { nome, livro, preco } = this.state;
+        const {nome, livro, preco} = this.state;
 
         return (
-            <form>
-                <Grid container spacing={2} alignItems={'center'}>
-                    <Grid item>
-                        <TextField
-                            variant={"outlined"}
-                            id={'nome'}
-                            name={'nome'}
-                            label={'Nome'}
-                            value={nome}
-                            onChange={this.escutadorDeInput}
-                        />
+            <>
+                <Toast
+                    open={this.state.mensagem.open}
+                    handleClose={() =>
+                        this.setState({mensagem: {open: false}})
+                    }
+                    severity={this.state.mensagem.tipo}
+                >
+                    {this.state.mensagem.mensagem}
+                </Toast>
+                <form>
+                    <Grid container spacing={2} alignItems={'center'}>
+                        <Grid item>
+                            <TextField
+                                variant={"outlined"}
+                                id={'nome'}
+                                name={'nome'}
+                                label={'Nome'}
+                                value={nome}
+                                onChange={this.escutadorDeInput}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                variant={"outlined"}
+                                id={'livro'}
+                                name={'livro'}
+                                label={'Livro'}
+                                value={livro}
+                                onChange={this.escutadorDeInput}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                variant={"outlined"}
+                                id={'preco'}
+                                name={'preco'}
+                                label={'Preço'}
+                                value={preco}
+                                onChange={this.escutadorDeInput}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant={"contained"}
+                                color={"primary"}
+                                onClick={() => this.submitFormulario()}
+                            >
+                                Salvar
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <TextField
-                            variant={"outlined"}
-                            id={'livro'}
-                            name={'livro'}
-                            label={'Livro'}
-                            value={livro}
-                            onChange={this.escutadorDeInput}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            variant={"outlined"}
-                            id={'preco'}
-                            name={'preco'}
-                            label={'Preço'}
-                            value={preco}
-                            onChange={this.escutadorDeInput}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Button
-                            variant={"contained"}
-                            color={"primary"}
-                            onClick={() => this.submitFormulario()}
-                        >
-                            Salvar
-                        </Button>
-                    </Grid>
-                </Grid>
-            </form>
+                </form>
+            </>
         );
     }
 }
+
 export default Formulario;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import './Home.css';
 import Header from '../../Components/Header/Header';
@@ -9,82 +9,88 @@ import ApiService from '../../utils/ApiService';
 
 class Home extends Component {
 
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      autores: [],
-    };
-  }
+        this.state = {
+            autores: [],
+        };
+    }
 
-  removeAutor = id => {
+    removeAutor = id => {
 
-    const { autores } = this.state;
+        const {autores} = this.state;
 
-    const autoresAtualizado = autores.filter(autor => {
-        return autor.id !== id;
-    });
-    ApiService.RemoveAutor(id)
-              .then(res =>{
-                if(res.message === 'deleted'){
-                  this.setState({autores : [...autoresAtualizado]})
-                  PopUp.exibeMensagem("error", "Autor removido com sucesso");
+        const autoresAtualizado = autores.filter(autor => {
+            return autor.id !== id;
+        });
+        ApiService.RemoveAutor(id)
+            .then(res => {
+                if (res.message === 'deleted') {
+                    this.setState({autores: [...autoresAtualizado]})
+                    PopUp.exibeMensagem("error", "Autor removido com sucesso");
                 }
-              })
-              .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar remover o autor"));
+            })
+            .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar remover o autor"));
 
-  }
+    }
 
-  escutadorDeSubmit = autor => {
-    ApiService.CriaAutor(JSON.stringify(autor))
-              .then(res =>{
-                if(res.message === 'success'){
-                  this.setState({ autores:[...this.state.autores, res.data] });
-                  PopUp.exibeMensagem("success", "Autor adicionado com sucesso");
+    escutadorDeSubmit = dados => {
+        const autor = {
+            nome: dados.nome,
+            livro: dados.livro,
+            preco: dados.preco
+        }
+
+        ApiService.CriaAutor(JSON.stringify(autor))
+            .then(res => {
+                if (res.message === 'success') {
+                    this.setState({autores: [...this.state.autores, res.data]});
+                    PopUp.exibeMensagem("success", "Autor adicionado com sucesso");
                 }
 
-              })
-              .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar criar o autor"));
+            })
+            .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar criar o autor"));
 
 
-  }
+    }
 
 
-  componentDidMount(){
-    ApiService.ListaAutores()
-                .then(res => {
-                  if(res.message === 'success'){
-                    this.setState({autores : [...this.state.autores, ...res.data]})
-                  }
+    componentDidMount() {
+        ApiService.ListaAutores()
+            .then(res => {
+                if (res.message === 'success') {
+                    this.setState({autores: [...this.state.autores, ...res.data]})
+                }
 
-                })
-                .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar listar os autores"));
-  }
+            })
+            .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao tentar listar os autores"));
+    }
 
-  getCampos = () => {
-      return [
-          { titulo: 'Autores', campo: 'nome' },
-          { titulo: 'Livros', campo: 'livro' },
-          { titulo: 'Preços', campo: 'preco' }
-      ]
-  }
+    getCampos = () => {
+        return [
+            {titulo: 'Autores', campo: 'nome'},
+            {titulo: 'Livros', campo: 'livro'},
+            {titulo: 'Preços', campo: 'preco'}
+        ]
+    }
 
-  render() {
+    render() {
 
-    return (
-      <Fragment>
-        <Header />
-        <div className="container mb-10">
-        <h1>App de livros</h1>
-        <Form escutadorDeSubmit={this.escutadorDeSubmit}/>
-        <Tabela
-            campos={this.getCampos()}
-            dados={this.state.autores}
-            removerDados={this.removeAutor} />
-        </div>
-      </Fragment>
-    );
-  }
+        return (
+            <Fragment>
+                <Header/>
+                <div className="container mb-10">
+                    <h1>App de livros</h1>
+                    <Form escutadorDeSubmit={this.escutadorDeSubmit}/>
+                    <Tabela
+                        campos={this.getCampos()}
+                        dados={this.state.autores}
+                        removerDados={this.removeAutor}/>
+                </div>
+            </Fragment>
+        );
+    }
 
 }
 
