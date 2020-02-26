9e3f2db1dd20cdbbe298f75e7f7ed0ecf3b3d0fc
jessykalage <jessyka.l@thoughtworks.com>
2020-02-26 12:50:43 -0300

    improve sobre page style

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,12 +1,27 @@
 import React, { Fragment } from 'react';
 import Header from '../../Components/Header/Header';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import {makeStyles} from "@material-ui/core/styles";
+
+const sobreStyle = makeStyles({
+    titulo: {
+        textAlign: 'center',
+        color: '#7986cb'
+    }
+});
 
 const Sobre = () =>{
-
+    const classes = sobreStyle();
     return (
         <Fragment>
             <Header />
-            <h1>Sobre</h1>
+            <Container maxWidth={"sm"}>
+                <Typography className={classes.titulo} variant={'h1'} component={'h2'}>
+                    Sobre
+                </Typography>
+                <Typography variant={'body1'} component={'p'}>Produzimos e editamos livros em diversos formatos.</Typography>
+            </Container>
         </Fragment>
     );
 }
